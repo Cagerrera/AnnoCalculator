2081aa1ad12736bf27c968ea6f6e9ffd53b84642
Cagerrera <rothuntgordwow@yandex.ru>
2024-10-18 01:59:17 +0300

    Создал конструктор API и объявли необходимые глобальные переменные

---
+++ AnnoCalculator/API.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnoCalculator
+{
+    internal class API
+    {
+        public int[] populationCount { get; private set; }
+        IntPtr gameHandle;
+        public API(IntPtr gameHandle) 
+        {
+            populationCount = new int[7];
+            this.gameHandle = gameHandle;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out IntPtr lpNumberOfBytesRead);
+        public void ReadDataFromGame()
+        {
+            
+        }
+
+    }
+
+}
